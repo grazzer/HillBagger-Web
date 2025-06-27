87fa091eb22d3c25504348a50f555004a0e44ec0
grazzer <edwardsgraham@hotmail.co.uk>
2025-06-27 20:03:53 +0100

    google maps AdvancedMarker

--- src/components/detailsPage/mapComponent.tsx
+++ src/components/detailsPage/mapComponent.tsx
@@ -1,4 +1,12 @@
-import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import {
+  APIProvider,
+  Map,
+  Marker,
+  Pin,
+  AdvancedMarker,
+  useMap,
+} from "@vis.gl/react-google-maps";
+import { useCallback, useState } from "react";
 
 export default function MapComponent({
   lati,
@@ -7,28 +15,50 @@ export default function MapComponent({
   lati: number;
   long: number;
 }) {
-  return (
-    <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
-      <div className="pb-5 pt-3">
-        <div className=" flex-col ">
-          <p className="inline ml-3 px-1.5 bg-white">Map</p>
+  const map = useMap();
 
-          <div className="flex-1 pt-1 aspect-square w-full max-h-screen">
-            <Map
-              style={{
-                borderRadius: "12px",
-                overflow: "hidden",
-              }}
-              //   style={{ width: "100vw", height: "100vh" }}
-              defaultCenter={{ lat: lati, lng: long }}
-              defaultZoom={12}
-              gestureHandling={"greedy"}
-              disableDefaultUI={true}
-            />
-          </div>
+  function handleClick() {
+    console.log("test");
+    map?.panTo({ lat: lati, lng: long });
+  }
+
+  return (
+    // <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+    <div className="pb-5 pt-3">
+      <div className=" flex-col ">
+        <p className="inline ml-3 px-1.5 bg-white">Map</p>
+
+        <div className="flex-1 pt-1 aspect-square w-full max-h-screen">
+          <Map
+            style={{
+              borderRadius: "12px",
+              overflow: "hidden",
+            }}
+            //   style={{ width: "100vw", height: "100vh" }}
+            defaultCenter={{ lat: lati, lng: long }}
+            defaultZoom={12}
+            gestureHandling={"greedy"}
+            disableDefaultUI={true}
+            mapId={"NEAR_ME_MAP_ID"}
+            reuseMaps={true}
+          >
+            <AdvancedMarker
+              position={{ lat: lati, lng: long }}
+              clickable={true}
+              onClick={handleClick}
+            >
+              <Pin
+                background={"#52d0b8"}
+                borderColor={"#3ea390"}
+                glyphColor={"#3ea390"}
+              />
+            </AdvancedMarker>
+            {/* <Marker position={{ lat: lati, lng: long }} /> */}
+          </Map>
         </div>
       </div>
-    </APIProvider>
+    </div>
+    // </APIProvider>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { routeTree } from "./routeTree.gen.ts";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
-
 import { StyledEngineProvider } from "@mui/material/styles";
+import { APIProvider } from "@vis.gl/react-google-maps";
 
 // Create a new router instance
 const router = createRouter({ routeTree, context: { hills: [] } });
@@ -23,7 +23,9 @@ if (!rootElement.innerHTML) {
   const root = createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+        <RouterProvider router={router} />
+      </APIProvider>
     </StrictMode>
   );
 }
